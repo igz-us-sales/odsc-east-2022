fe8abd163f7a137968856baaaf619089bdde6aed
marcelo <kdxlmo2#!LL>
2022-04-15 16:30:58 -0400

    Remote example

--- odsc-02.py
+++ odsc-02.py
@@ -1,9 +1,14 @@
-from mlrun import code_to_function
+import mlrun
+from os import path
 
-fn = code_to_function(kind='job',
+project_name_base = 'odsc-2022-east'
+project_name, artifact_path = mlrun.set_environment(project=project_name_base, artifact_path='./', user_project=True)
+
+fn = mlrun.code_to_function(kind='job',
                       filename='data_iris.py',
                       handler='iris_generator',
                       image='mlrun/mlrun')
 
 params={"format":"parquet"}
-fn.run(params=params)
+fn.run(params=params,
+       artifact_path=path.join(artifact_path, 'data'))
