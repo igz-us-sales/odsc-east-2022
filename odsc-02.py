30432cc73bb0a14a8fc7c7d1f7856b5678cbbb09
marcelo <kdxlmo2#!LL>
2022-04-27 16:04:21 -0400

    Added an environment file

--- odsc-02.py
+++ odsc-02.py
@@ -1,6 +1,8 @@
 import mlrun
 from os import path
 
+mlrun.set_env_from_file(".env")
+
 project_name_base = 'odsc-2022-east'
 project_name, artifact_path = mlrun.set_environment(project=project_name_base, artifact_path='./', user_project=True)
 
